0c0412b1437a50fe26aa4610b1079a3bad9f197f
Goutam Sirsikar <goutam.sirsikar@gmail.com>
2021-08-01 16:24:09 +0530

    ec2 instance with SG creation with remote state file test1

--- shell-scripting-main/terraform/robo2/ec2.tf
+++ shell-scripting-main/terraform/robo2/ec2.tf
@@ -2,7 +2,7 @@ resource "aws_spot_instance_request" "myresource" {
   count =  length(var.COMPONENTS)
   ami = "ami-074df373d6bafa625"
   instance_type = "t3.micro"
-  depends_on = "security"
+  depends_on = [aws_security_group.roboshop_sg.name]
   vpc_security_group_ids = aws_security_group.roboshop_sg.id  // ["sg-0980c269147cd7819"]
   wait_for_fulfillment = true
   tags = {
