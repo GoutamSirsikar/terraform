97dde3c60adb3269814bca13deff6edd54d4bd4a
Goutam Sirsikar <goutam.sirsikar@gmail.com>
2021-08-01 12:49:22 +0530

    ec2 instance with tag

--- shell-scripting-main/terraform/ec2.tf
+++ shell-scripting-main/terraform/ec2.tf
@@ -5,10 +5,15 @@ resource "aws_spot_instance_request" "myresource" {
   vpc_security_group_ids = ["sg-0980c269147cd7819"]
   wait_for_fulfillment = true
   tags = {
-    Name = element(var.COMPONENTS,count.index)
+    Name = element(var.COMPONENTS, count.index)
   }
   }
-
+resource "aws_ec2_tag" "my-tag" {
+  count = length(var.COMPONENTS)
+  key = "Name"
+  resource_id = element(aws_spot_instance_request.myresource.*.spot_instance_id,count.index)
+  value = element (var.COMPONENTS,count.index)
+}
 
 provider "aws" {
   region = "us-east-1"
