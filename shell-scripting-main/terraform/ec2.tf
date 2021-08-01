b3b877228e5bedf7c01ef1258cee5b8804dcb875
Goutam Sirsikar <goutam.sirsikar@gmail.com>
2021-08-01 15:34:54 +0530

    ec2 instance with SG creation test1

--- shell-scripting-main/terraform/ec2.tf
+++ shell-scripting-main/terraform/ec2.tf
@@ -2,12 +2,38 @@ resource "aws_spot_instance_request" "myresource" {
   count =  length(var.COMPONENTS)
   ami = "ami-074df373d6bafa625"
   instance_type = "t3.micro"
-  vpc_security_group_ids = ["sg-0980c269147cd7819"]
+  vpc_security_group_ids = aws_security_group.roboshop_sg.id  // ["sg-0980c269147cd7819"]
   wait_for_fulfillment = true
   tags = {
     Name = element(var.COMPONENTS, count.index)
   }
   }
+
+resource "aws_security_group" "roboshop_sg" {
+  name        = "roboshop_sg"
+  description = "Allow  traffic"
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 0
+    to_port          = 0
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "roboshop-SG"
+  }
+}
 resource "aws_ec2_tag" "my-tag" {
   count = length(var.COMPONENTS)
   key = "Name"
