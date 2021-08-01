2302f77dabf2d64ebd93b7b41c6d0a9737d41657
Goutam Sirsikar <goutam.sirsikar@gmail.com>
2021-08-01 12:00:44 +0530

    ec2 instance with tag

--- shell-scripting-main/terraform/ec2.tf
+++ shell-scripting-main/terraform/ec2.tf
@@ -1,10 +1,13 @@
 resource "aws_spot_instance_request" "myresource" {
   ami = "ami-074df373d6bafa625"
   instance_type = "t3.micro"
-  tags = {
-    Name    = "MyInstance"
   }
+
+resource "aws_ec2_tag" "myinstance" {
+  Name     = "myinistance"
 }
+
+
 provider "aws" {
   region = "us-east-1"
 }
