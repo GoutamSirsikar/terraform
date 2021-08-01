e422be2daa4c81779704a1199726417b1362fc62
Goutam Sirsikar <goutam.sirsikar@gmail.com>
2021-08-01 12:13:41 +0530

    ec2 instance with tag

--- shell-scripting-main/terraform/ec2.tf
+++ shell-scripting-main/terraform/ec2.tf
@@ -1,11 +1,12 @@
 resource "aws_spot_instance_request" "myresource" {
+  count =  3
   ami = "ami-074df373d6bafa625"
   instance_type = "t3.micro"
+  vpc_security_group_id = [sg-0980c269147cd7819]
+  tags = {
+    Name = "myinistance"
   }
-
-resource "aws_ec2_tag" "myinstance" {
-  Name     = "myinistance"
-}
+  }
 
 
 provider "aws" {
