7d22ac5ec28bc87990aff95add79511c6dcb9670
Goutam Sirsikar <goutam.sirsikar@gmail.com>
2021-08-02 17:03:20 +0530

    doing test to create a folder

--- shell-scripting-main/terraform/robo2/ec2.tf
+++ shell-scripting-main/terraform/robo2/ec2.tf
@@ -72,10 +72,11 @@ resource "null_resource" "run-shell-scripting" {
 
     inline = [
       "cd /home/centos",
-      "git clone https://DevOps-Batches@dev.azure.com/DevOps-Batches/DevOps57/_git/terraform",
-      "cd shell-scripting/roboshop",
-      "git pull",
-      "sudo make ${element(var.COMPONENTS, count.index)}"
+      "mkdir gomsy"
+//      "git clone https://DevOps-Batches@dev.azure.com/DevOps-Batches/DevOps57/_git/terraform",
+//      "cd shell-scripting/roboshop",
+//      "git pull",
+//      "sudo make ${element(var.COMPONENTS, count.index)}"
     ]
   }
 }
