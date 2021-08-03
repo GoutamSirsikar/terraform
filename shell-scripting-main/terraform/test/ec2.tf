b8181e432fbe3123fa1ccfa996980d66ecbd93c7
Goutam Sirsikar <goutam.sirsikar@gmail.com>
2021-08-03 18:18:56 +0530

    test

--- shell-scripting-main/terraform/test/ec2.tf
+++ shell-scripting-main/terraform/test/ec2.tf
@@ -2,7 +2,7 @@ resource "aws_spot_instance_request" "mytest" {
   count =  length(var.COMPO)
   ami = "ami-074df373d6bafa625"
   instance_type = "t3.micro"
-  vpc_security_group_ids = sg-0980c269147cd7819
+  vpc_security_group_ids = ["sg-0980c269147cd7819"]
   wait_for_fulfillment = true
   tags = {
     Name = element(var.COMPO, count.index)
@@ -12,7 +12,7 @@ provider "aws" {
   region = "us-east-1"
 }
 output "PUBLIC_IP" {
-  value = aws_spot_instance_request.mytest.*.associate_public_ip_address
+  value = element(aws_spot_instance_request.mytest.*.associate_public_ip_address,count.index[1] )
 }
 
 variable "COMPO" {}
