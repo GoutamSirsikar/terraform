c64792124b5e2b0451ee6f0d43f9c9cd52ee13e3
Goutam Sirsikar <goutam.sirsikar@gmail.com>
2021-08-03 18:34:12 +0530

    test2

--- shell-scripting-main/terraform/test/ec2.tf
+++ shell-scripting-main/terraform/test/ec2.tf
@@ -12,7 +12,7 @@ provider "aws" {
   region = "us-east-1"
 }
 output "PUBLIC_IP" {
-  value = aws_spot_instance_request.mytest.id[0]
+  value = aws_spot_instance_request.mytest.private_ip[0]
 }
 
 variable "COMPO" {}
